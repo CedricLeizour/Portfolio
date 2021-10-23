ba95292af04cf174c7f38f459c9a64ada6bfd62a
CedricLeizour <cedricleizour@gmail.com>
2021-10-23 14:00:26 +0200

    test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 // == Import npm
 import React from 'react';
 import { GitHub, LinkedIn, AssignmentInd } from '@material-ui/icons';
-import CV from '../../assets/documents/CV Cédric Leizour.pdf';
+import CV from '../../assets/documents/CV Cedric Leizour.pdf';
 
 // == Import
 import './styles.scss';
